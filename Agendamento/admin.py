a0cae1840f1bb7ac776516d1b7534f68966137e6
Kelvin-Acer <Kelvin@DESKTOP-43R6UIP>
2024-01-04 22:29:26 -0300

    Criando view e template para grafico em barra e ajustando alguns filtros do html

--- Agendamento/admin.py
+++ Agendamento/admin.py
@@ -1,8 +1,55 @@
 from django.contrib import admin
-from .models import Estabelecimento
+from .models import Estabelecimento, Agendamento, AgendamentoUsuario
+from django.urls import path
+from django.shortcuts import render
+from django.db.models import Count
 
+
+
+@admin.register(Estabelecimento)
 class EstabelecimentoAdmin(admin.ModelAdmin):
     list_display = ('nome_estabelecimento', 'codigo_cnes')
     search_fields = ['nome_estabelecimento', 'codigo_cnes']
 
-admin.site.register(Estabelecimento, EstabelecimentoAdmin)
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_filter = ('estabelecimento', 'data_agendamento',)
+    list_display = ('estabelecimento', 'data_agendamento',)
+    search_fields = ('estabelecimento', 'data_agendamento',)
+    ordering = ('estabelecimento', 'data_agendamento',)
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('graficos/', self.admin_site.admin_view(self.grafico_view), name='agendamento-graficos'),
+        ]
+        return custom_urls + urls
+
+    def grafico_view(self, request):
+        # Aqui você vai gerar os dados para o seu gráfico
+        context = self.get_grafico_context()
+        return render(request, 'grafico_agendamentos.html', context)
+
+    def get_grafico_context(self):
+        # Aqui você gera os dados para o gráfico, isso é apenas um exemplo
+        agendamentos_por_estabelecimento = (
+            Agendamento.objects
+            .values('estabelecimento__nome_estabelecimento')
+            .annotate(total=Count('id'))
+            .order_by('-total')
+        )
+        labels = [agendamento['estabelecimento__nome_estabelecimento'] for agendamento in agendamentos_por_estabelecimento]
+        data = [agendamento['total'] for agendamento in agendamentos_por_estabelecimento]
+        return {'labels': labels, 'data': data}
+
+
+@admin.register(AgendamentoUsuario)
+class AgendamentoUsuarioAdmin(admin.ModelAdmin):
+    list_filter = ('agendamento__estabelecimento', 'usuario', 'is_active',) 
+    list_display = ('agendamento', 'usuario', 'get_hora_agendamento', 'is_active',)
+    search_fields = ('agendamento__estabelecimento__nome_estabelecimento', 'usuario__username',)
+    ordering = ('agendamento__estabelecimento', 'usuario',)  
+
+    def get_hora_agendamento(self, obj):
+        return obj.agendamento.hora_agendamento
+    get_hora_agendamento.admin_order_field = 'agendamento__hora_agendamento' 
+    get_hora_agendamento.short_description = 'Hora do Agendamento'
--- Agendamento/views.py
+++ Agendamento/views.py
@@ -5,9 +5,8 @@ from django.shortcuts import redirect
 from django.contrib import messages
 from .models import Estabelecimento, AgendamentoUsuario, Agendamento
 from datetime import datetime
-from django.shortcuts import get_object_or_404
 from django.utils import timezone
-from datetime import timedelta
+from django.urls import reverse
 
 
 
@@ -62,37 +61,44 @@ def home(request):
 
 
 @login_required(login_url="/auth/login/")
-def realizar_agendamento(request, agendamento_id=None):
+def realizar_agendamento(request):
     usuario = request.user
+    estabelecimentos = Estabelecimento.objects.all()
 
-    agendamento_direto = None
-    if agendamento_id:
-        agendamento_direto = get_object_or_404(Agendamento, id=agendamento_id)
+    if request.method == 'GET':
+        query = request.GET.get('q', '')
+        filtro = request.GET.get('filtro', 'nome')
+        if filtro == 'nome':
+            estabelecimentos = estabelecimentos.filter(nome_estabelecimento__icontains=query)
+        elif filtro == 'cnes':
+            estabelecimentos = estabelecimentos.filter(codigo_cnes__icontains=query)
+    else:
+        estabelecimento_id = request.POST.get('estabelecimento')
+        data_agendamento_str = request.POST.get('data_agendamento')
+        hora_agendamento = request.POST.get('hora_agendamento')
 
-    if request.method == 'POST' or agendamento_direto:
-        agendamento = agendamento_direto
-
-        if not agendamento:
-            estabelecimento_id = request.POST.get('estabelecimento')
-            data_agendamento_str = request.POST.get('data_agendamento')
+        try:
             data_agendamento = datetime.strptime(data_agendamento_str, '%Y-%m-%d').date()
-            hora_agendamento = request.POST.get('hora_agendamento')
-            dia_da_semana = data_agendamento.weekday()
+        except ValueError:
+            messages.error(request, "Formato de data inválido.")
+            return redirect('home')
 
-            if data_agendamento < (timezone.now().date() + timedelta(days=1)):
-                messages.error(request, "Por favor, selecione uma data futura para o agendamento.")
-                return redirect('home')
-            
-            if dia_da_semana < 2 or dia_da_semana > 5:
-                messages.error(request, "Agendamentos são permitidos apenas de quarta a sábado.")
-                return redirect('home')
+        dia_da_semana = data_agendamento.weekday()
 
-            agendamento, created = Agendamento.objects.get_or_create(
-                estabelecimento_id=estabelecimento_id, 
-                data_agendamento=data_agendamento, 
-                hora_agendamento=hora_agendamento,
-                defaults={'vagas_disponiveis': 5} 
-            )
+        if data_agendamento < timezone.now().date():
+            messages.error(request, "Por favor, selecione uma data futura para o agendamento.")
+            return redirect('home')
+        
+        if dia_da_semana < 2 or dia_da_semana > 5:
+            messages.error(request, "Agendamentos são permitidos apenas de quarta a sábado.")
+            return redirect('home')
+
+        agendamento, created = Agendamento.objects.get_or_create(
+            estabelecimento_id=estabelecimento_id, 
+            data_agendamento=data_agendamento, 
+            hora_agendamento=hora_agendamento,
+            defaults={'vagas_disponiveis': 5} 
+        )
 
         if agendamento.vagas_disponiveis > 0:
             AgendamentoUsuario.objects.filter(usuario=usuario, is_active=True).update(is_active=False)
@@ -100,18 +106,17 @@ def realizar_agendamento(request, agendamento_id=None):
             agendamento.vagas_disponiveis -= 1
             agendamento.save()
             messages.success(request, "Agendamento realizado com sucesso.")
-            return redirect('home')
+            return redirect(reverse('home'))
         else:
             messages.error(request, "Não há vagas disponíveis para este horário.")
+            return redirect(reverse('home'))
 
     estabelecimentos = Estabelecimento.objects.all()
     context = {
-        'agendamento_direto': agendamento_direto,
         'estabelecimentos': estabelecimentos,
     }
 
     return render(request, 'home.html', context)
-
 
 @login_required(login_url="/auth/login/")
 def lista_agendamento(request):
